b3080246dc8632eced6cbb914fc17fe1e1a46f25
volodymyr.momot <volodymyr.momot@itomy.ch>
2022-11-30 20:54:02 +0200

    update

---
+++ src/entities/roles/abstract_role.ts
@@ -0,0 +1,37 @@
+import Effect from "../effects/base_effect";
+
+
+export default abstract class BaseRole {
+   private health: number;
+   private isAlive: boolean = true;
+   private effects: Effect[] = [];
+
+    protected attack(target: BaseRole, damage: number): void {
+        if (target === this) {
+            throw new Error('Cannot attack self');
+        }
+        if (damage < 0) {
+            throw new Error('Damage cannot be negative');
+        }
+        target.takeDamage(damage);
+    }
+
+    protected setHealth(health: number): void {
+        this.health = health;
+    }
+
+    takeDamage(damage: number): void {
+        this.health = this.health - damage >= 0 ? this.health - damage : 0;
+    }
+
+    setEffect(effect: Effect): void {
+        if (this.effects.includes(effect)) {
+            this.deleteEffect(effect);
+        }
+        this.effects.push(effect);
+    }
+
+    deleteEffect(effect: Effect): void {
+        this.effects = this.effects.filter((e) => e !== effect);
+    }
+}
