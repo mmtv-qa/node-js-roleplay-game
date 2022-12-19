b659bcc663f1c01da0a239f4a7850c157fc0eb40
volodymyr.momot <volodymyr.momot@itomy.ch>
2022-12-19 14:32:38 +0200

    change method

--- src/servers/http-server.ts
+++ src/servers/http-server.ts
@@ -1,8 +1,11 @@
+import {Request, Response} from "express";
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/echo', (req, res) => {
+app.post('/echo', (req: Request, res: Response) => {
+    res.status(200)
     res.send(req.body)
 })
 
